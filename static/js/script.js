47704e83928975d2ec80d0a19fd587da11c21194
Всеволод <63463168+vsecoder@users.noreply.github.com>
2021-12-05 17:14:01 +0300

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,17 @@
 new WOW().init();
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
